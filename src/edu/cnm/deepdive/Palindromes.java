ffc9e9096b401365011671cd48bf53897109d641
Steven Zuniga <szuniga@cnm.edu>
2020-02-03 10:17:14 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/Palindromes.java
@@ -0,0 +1,18 @@
+package edu.cnm.deepdive;
+
+public final class Palindromes {
+
+  private Palindromes() {}
+
+  private static boolean isPalindromeClean(String source) {
+    return source.length() <= 1
+        || (
+            source.charAt( 0 ) == source.charAt( source.length() - 1 )
+            && isPalindromeClean( source.substring( 1, source.length() - 1 ) )
+        );
+  }
+
+  public static boolean isPalindrome(String source) {
+    
+  }
+}
