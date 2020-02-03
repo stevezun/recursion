cee515a65de0703a375424af773671e30985db56
Steven Zuniga <szuniga@cnm.edu>
2020-02-03 11:47:37 -0700

    Created parameterized test method.

--- test/edu/cnm/deepdive/PalindromesTest.java
+++ test/edu/cnm/deepdive/PalindromesTest.java
@@ -3,38 +3,24 @@ package edu.cnm.deepdive;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class PalindromesTest {
 
-  static final String[] positiveTestData = {
-      "racecar",
-      "Race Car",
-      "radar",
-      "",
-      "x",
-      "Xx",
-      "A man, a plan, a canal - Panama!"
-  };
 
-  static final String[] negativeTestData = {
-      "xy",
-      "sonar",
-      "Madame, I'm Adam",
-      "Burrito Dog"
-  };
 
-  @Test
-  void isPalindromeAffirmative() {
-    for (String testCase : positiveTestData) {
-      assertTrue(Palindromes.isPalindrome(testCase));
-    }
+  @ParameterizedTest
+  @CsvFileSource(resources = "/affirmative.csv")
+  void isPalindromeAffirmative(String testCase) {
+    assertTrue(Palindromes.isPalindrome(testCase));
   }
 
-  @Test
-  void isPalindromeNegative() {
-    for (String testCase : negativeTestData) {
-      assertFalse(Palindromes.isPalindrome(testCase));
-    }
+  @ParameterizedTest
+  @CsvFileSource(resources = "/negative.csv")
+  void isPalindromeNegative(String testCase) {
+    assertFalse(Palindromes.isPalindrome(testCase));
   }
 
 }
